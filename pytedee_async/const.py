074de63d089df227b28c7c0345335c0d8d3c2320
Josef Zweck <josef.zweck@microsoft.com>
2023-06-25 20:40:28 +0200

    add const

---
+++ pytedee_async/const.py
@@ -0,0 +1,13 @@
+API_URL_BASE = "https://api.tedee.com/api/v1.30/"
+API_URL_DEVICE = API_URL_BASE + "my/device"
+API_URL_LOCK = API_URL_BASE + "my/lock/"
+API_URL_STATE = API_URL_BASE + "my/lock/sync"
+
+API_PATH_UNLOCK = "/operation/unlock"
+API_PATH_LOCK = "/operation/lock"
+API_PATH_PULL = "/operation/pull"
+API_PATH_BATTERY = "/battery"
+
+TIMEOUT = 10
+UNLOCK_DELAY = 5
+LOCK_DELAY = 5

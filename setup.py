aad8eb955bf41d550a2419b2f65c83d3e5836890
Josef Zweck <josef.zweck@microsoft.com>
2023-06-25 20:40:14 +0200

    update setup

--- setup.py
+++ setup.py
@@ -9,19 +9,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pytedee", # Replace with your own username
-    package_dir = {'': 'pytedee'},
-    packages = ['pytedee'],
-    version="0.0.3",
-    author="Jörg Wolff",
-    author_email="joerg.wolff@gmx.de",
+    name="pytedee_async", 
+    version="0.0.4",
+    author="Josef Zweck",
+    author_email="24647999+zweckj@users.noreply.github.com",
     description="A Tedee Lock Client package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/joerg65/pytedee",
-    #packages=setuptools.find_packages(),
-    install_requires=['requests'],
+    url="https://github.com/zweckj/pytedee",
+    packages=setuptools.find_packages(),
+    install_requires=['aiohttp', 'asyncio'],
+    license="MIT",
     classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
